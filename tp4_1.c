7e3a9e45ea9953540e3f2a36fcfff97e60dea3b2
Agustin Delgado <delgadoagustin147@gmail.com>
2020-05-18 17:22:25 -0300

    Add some code

--- tp4_1.c
+++ tp4_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct {
     int TareaID;
@@ -7,14 +8,36 @@ typedef struct {
     int Duracion;
 }Tarea;
 
+void cargar(Tarea ** arreglo, int n);
+
 int main(){
 
+    srand(time(NULL));
+
     int cantTareas;
+    Tarea ** tareasPendientes;
+    Tarea ** tareasRealizadas;
 
-    printf("Ingresa la cantidad de Tareas a cargar:");
+    printf("Ingresa la cantidad de Tareas a cargar: ");
     scanf("%d",cantTareas);
+    getchar();
+    tareasPendientes = (Tarea**)malloc(sizeof(Tarea)*cantTareas);
+    tareasRealizadas = (Tarea**)malloc(sizeof(Tarea)*cantTareas);
 
 
+    printf("\nOK");
     getchar(); //Stop
     return 0;
+}
+
+void cargar(Tarea ** arreglo, int n){
+    char descripcion[30];
+    for(int i=0;i<n;i++){
+        (*(arreglo+i))->TareaID = i+1;
+        printf("Igresa descripcion de la tarea: ");
+        gets(descripcion);
+        (*(arreglo+i))->Descripcion = (char*)malloc(sizeof(char)*30);
+        (*(arreglo+i))->Descripcion = descripcion;
+        (*(arreglo+i))->Duracion = rand()%101+10;
+    }
 }
