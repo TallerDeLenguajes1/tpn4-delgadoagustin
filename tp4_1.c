cf839da188e82b3fac03b8194bf46c7478c8accc
Agustin Delgado <delgadoagustin147@gmail.com>
2020-05-18 19:43:09 -0300

    Add cargarTarea() and mostrarTarea()

--- tp4_1.c
+++ tp4_1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+
+#define MAX 30
 
 typedef struct {
     int TareaID;
@@ -9,6 +12,7 @@ typedef struct {
 }Tarea;
 
 void cargar(Tarea ** arreglo, int n);
+void mostrarTarea(Tarea T);
 
 int main(){
 
@@ -19,25 +23,36 @@ int main(){
     Tarea ** tareasRealizadas;
 
     printf("Ingresa la cantidad de Tareas a cargar: ");
-    scanf("%d",cantTareas);
+    scanf("%d",&cantTareas);
     getchar();
     tareasPendientes = (Tarea**)malloc(sizeof(Tarea)*cantTareas);
     tareasRealizadas = (Tarea**)malloc(sizeof(Tarea)*cantTareas);
 
+    cargar(tareasPendientes,cantTareas);
 
-    printf("\nOK");
+    for(int i=0;i<cantTareas;i++){
+        mostrarTarea(*tareasPendientes[i]);
+    }
+
     getchar(); //Stop
     return 0;
 }
 
 void cargar(Tarea ** arreglo, int n){
-    char descripcion[30];
+    char descripcion[MAX];
     for(int i=0;i<n;i++){
-        (*(arreglo+i))->TareaID = i+1;
+        arreglo[i] = (Tarea*)malloc(sizeof(Tarea));
+        arreglo[i]->TareaID = i+1;
         printf("Igresa descripcion de la tarea: ");
         gets(descripcion);
-        (*(arreglo+i))->Descripcion = (char*)malloc(sizeof(char)*30);
-        (*(arreglo+i))->Descripcion = descripcion;
-        (*(arreglo+i))->Duracion = rand()%101+10;
+        arreglo[i]->Descripcion = (char*)malloc(sizeof(char)*MAX);
+        strcpy(arreglo[i]->Descripcion,descripcion);
+        arreglo[i]->Duracion = rand()%101+10;
     }
+}
+
+void mostrarTarea(Tarea T){
+    printf("ID: %d\n",T.TareaID);
+    printf("%s\n",T.Descripcion);
+    printf("Duracion: %d\n\n",T.Duracion);
 }
